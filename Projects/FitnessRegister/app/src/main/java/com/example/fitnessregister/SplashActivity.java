4b7f463ecd235fe7eba55f41f83b6a1f6b64f9f3
AlexMnN <43807534+AlexMnN@users.noreply.github.com>
2019-01-31 19:26:26 +0000

    Added request WindowFeature

--- Projects/FitnessRegister/app/src/main/java/com/example/fitnessregister/SplashActivity.java
+++ Projects/FitnessRegister/app/src/main/java/com/example/fitnessregister/SplashActivity.java
@@ -4,11 +4,16 @@ import android.content.Intent;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
 
 public class SplashActivity extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
         Handler handler = new Handler();
